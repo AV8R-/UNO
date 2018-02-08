f5cf1ef05ed10d470e3ce97767c67ed561a77409
Bogdan Manshilin <skimphiter@gmail.com>
2018-02-09 01:21:29 +0200

    Initial Commit

---
+++ UNO/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  UNO
+//
+//  Created by Богдан Маншилин on 09/02/2018.
+//  Copyright © 2018 BManshilin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
