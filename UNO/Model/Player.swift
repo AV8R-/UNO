6c6232a65ae356ec5e7c295203d55c94a2596bc8
Bogdan Manshilin <skimphiter@gmail.com>
2018-02-09 02:06:44 +0200

    add: Calculator service interface

---
+++ UNO/Model/Player.swift
@@ -0,0 +1,13 @@
+//
+//  Player.swift
+//  UNO
+//
+//  Created by Богдан Маншилин on 09/02/2018.
+//  Copyright © 2018 BManshilin. All rights reserved.
+//
+
+import Foundation
+
+public struct Player {
+    public var name: String
+}
---
+++ UNO/Services/Services.h
@@ -0,0 +1,19 @@
+//
+//  Services.h
+//  Services
+//
+//  Created by Богдан Маншилин on 09/02/2018.
+//  Copyright © 2018 BManshilin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Services.
+FOUNDATION_EXPORT double ServicesVersionNumber;
+
+//! Project version string for Services.
+FOUNDATION_EXPORT const unsigned char ServicesVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Services/PublicHeader.h>
+
+
