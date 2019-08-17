385dae4198f7c5ce696390fccd3b4da547aeeb8e
Bogdan Manshilin <skimphiter@gmail.com>
2019-08-17 15:27:04 +0200

    add: tests for addPlayersViewModel

--- UNOTests/AddPlayersViewModelSpec.swift
+++ UNOTests/AddPlayersViewModelSpec.swift
@@ -16,12 +16,32 @@ final class AddPlayersViewModelSpec: QuickSpec {
         }
         
         describe(".appendPlayer(name:)") {
-            it("should not append empty name") {
+            it("should throw when appending empty name") {
                 let playerName = ""
                 
                 expect {
                     try viewModel.appendPlayer(name: playerName)
                 }.to(throwError(AddPlayerViewModelError.playerNameShouldNotBeEmpty))
+            }
+            
+            it("should throw when appending existing name") {
+                let player = Stubs.player1
+
+                viewModel.players.append(player)
+                
+                expect {
+                    try viewModel.appendPlayer(name: player.name)
+                }.to(throwError(AddPlayerViewModelError.playerAlreadyExist))
+            }
+            
+            it("should append valid player") {
+                let player = Stubs.player1
+                
+                expect {
+                    try viewModel.appendPlayer(name: player.name)
+                }.toNot(throwError())
+                
+                expect(viewModel.players.array).to(contain(player))
             }
         }
         
